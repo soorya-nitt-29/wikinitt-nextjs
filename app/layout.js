92a7760b287b58560e0d4ed9cdc26209a41c5556
Soorya S <sooryanitt25@gmail.com>
2026-01-18 01:48:17 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
 import './globals.css'
 
-export const metadata = {
-  title: 'WikiNITT',
-  description: 'NIT Trichy Encyclopedia',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
