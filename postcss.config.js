670371ff203eca3a198e4295f51fefbfbb04790d
Soorya S <sooryanitt25@gmail.com>
2026-01-17 23:38:23 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+**Commit this file**
+
+---
+
+### **File 5: `.gitignore`**
+
+**Filename:** `.gitignore`
+
+**Content:**
+```
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# next.js
+/.next/
+/out/
+
+# production
+/build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+# local env files
+.env*.local
+
+# vercel
+.vercel
+
+# typescript
+*.tsbuildinfo
+next-env.d.ts
